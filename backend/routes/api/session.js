2f8e301eedc82ce883fa138a8469577613c98ede
Brian Washington Jr <bw49078@gmail.com>
2023-09-20 13:10:38 -0500

    Incorrect password or email/username on login now sends a bad request error asking for correct data

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,27 @@
 // backend/routes/api/session.js
 const express = require('express');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -28,7 +41,7 @@ router.post(
         user: user
       });
     }
-);
+  );
 
 // Log out
 router.delete(
