9d92e16df830ec8641a2ad9e435e3e9a18c949b9
Brian Washington Jr <bw49078@gmail.com>
2023-09-20 12:38:15 -0500

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,6 +28,16 @@ router.post(
         user: user
       });
     }
-  );
+);
 
-  module.exports = router;
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
+  
+
+module.exports = router;
